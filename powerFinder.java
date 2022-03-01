34ed4b4603fadf35cfaff5074e81f407bc8af63a
Prapti <58839978+prapti1803@users.noreply.github.com>
2022-03-01 20:59:03 +0530

    Create powerFinder.java

---
+++ powerFinder.java
@@ -0,0 +1,12 @@
+package Power;
+public class powerFinder {
+	
+	    public static int OF(int xyz, int pqr) {
+	        int p = 1;
+	        for (int i = 0; i<pqr; i++) {
+	            p *= xyz;
+	        }
+	        return p;
+	    }
+	}
+
